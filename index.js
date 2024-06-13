ca7b74384f03ed009a1277740293a21e576791f8
despaqui9u <liamdespaqiis@gmail.com>
2024-06-13 11:15:33 +0200

    index.js

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Définir le dossier public comme source de fichiers statiques
-app.use(express.static(path.join(__dirname, 'public')));
+// Servir les fichiers statiques
+app.use(express.static(__dirname));
+
+// Route pour servir le fichier index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 // Démarrer le serveur
 app.listen(port, () => {
--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 import { basePathsApi } from "./const";
 console.log('k');
+/*
 fetch(basePathsApi+'entrees').then(ent => 
     ent.json().then( ent => {
         console.log(ent);
     })
-)
+)*/
